dd85611098b0b9ad467684b9b63d16884c37c679
Jon Goose <jongoose69@gmail.com>
2024-02-08 15:09:37 -0800

    iiiiiii

--- neuron.c
+++ neuron.c
@@ -7,13 +7,40 @@ typedef connection_list_node cln;
 struct neuron {
 	double bias;
 	double activation;
-	connection_list_node * connections;
+	connection_list_node * connections_head;
 };
 struct connection_list_node {
 	double weight;
 	neuron * from;
 	connection_list_node * next;
 };
+
+double evaluate(neuron * n){
+	/* tanh(sum(w*x) + b) */
+
+	double a;
+	int i;
+	cln * curr = n->connections_head;
+	a = n->bias;
+	while(curr){
+		a = a + curr->weight * curr->from->activation
+		curr = curr->next;
+	}
+
+	a = tanh(a);
+	*(n->activation) = a;
+	return a;
+}
+
+cln * add_connection(neuron * from, neuron * to,double weight){
+	/* caller frees */
+
+	cln * new_conn = malloc(sizeof(cln));
+	new_conn->weight = weight;
+	new_conn->from = from;
+	new_conn->next = to->connections_head;
+	to->connections_head = new_conn;
+}
 
 int main(void){
 	return 0;
