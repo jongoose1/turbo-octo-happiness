4a5b325743571e3a49726e96929be7fb3aec7da0
Jon Goose <jongoose69@gmail.com>
2024-02-07 21:29:02 -0800

    I have commitment issues D:

--- inator.c
+++ inator.c
@@ -1,5 +1,27 @@
+#include <time.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(void){
-	printf("hello, world\n");
+/* real -> real min finder */
+
+#define DELTA 0.1
+#define RATE 0.1
+
+double observe(double x){
+	double y;
+	printf("f(%lf) = ", x);
+	scanf("%lf", &y);
+	return y;
+}
+
+int main(int argc, char ** argv){
+	srand(time(NULL));
+	double guess = 0.0;
+	double slope, y2, y1;
+	while(1){
+		y1 = observe(guess);
+		y2 = observe(guess + DELTA);
+		slope = (y2- y1) / DELTA;
+		guess = guess - RATE * slope;
+	}
 }
